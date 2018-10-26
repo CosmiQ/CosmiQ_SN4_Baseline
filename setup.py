eb7943e9c8c0f6d03b9c76ee6189459c9e4c3f37
nrweir <nicholas.r.weir@gmail.com>
2018-10-26 16:54:14 -0400

    debugging console_scripts

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ setup(
 
     dependency_links=['https://github.com/SpaceNetChallenge/utilities.git@spacenetV3#egg=spacenetutilites-3.0'],
 
-    install_requires=['numpy>=1.15', 'pandas>=0.23.4', 'scikit-image>=0.14.0',
+    install_requires=['numpy', 'pandas', 'scikit-image',
                       'rasterio>=1.0.7', 'opencv>=3.4.3',
                       'spacenetutilities==3.0', 'keras>=2.2.2',
                       'tensorflow>=1.10.0', 'tensorboard>=1.10.0',
-                      'setuptools>=40.4.0'],  # TODO: UPDATE
+                      'setuptools'],  # TODO: UPDATE
     entry_points={
         'console_scripts': [
-            'make_np_arrays = bin/make_np_arrays:main_func',
-            'train_model = bin/train_model:main_func',
-            'make_predictions = bin/make_predictions:main_func'
+            'make_np_arrays=bin/make_np_arrays:main_func',
+            'train_model=bin/train_model:main_func',
+            'make_predictions=bin/make_predictions:main_func'
         ]
     },
 
