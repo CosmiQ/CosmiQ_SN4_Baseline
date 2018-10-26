beff2d63f97917b35a3ae233ec69e21bb82e67a0
nrweir <nicholas.r.weir@gmail.com>
2018-10-26 17:17:09 -0400

    working setup.py

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ setup(
     packages=find_packages(),
     include_package_data=True,
 
-    dependency_links=['https://github.com/SpaceNetChallenge/utilities.git@spacenetV3#egg=spacenetutilites-3.0'],
+    dependency_links=['https://github.com/SpaceNetChallenge/utilities/tarball/spacenetV3#egg=spacenetutilities-3.0'],
 
     install_requires=['numpy', 'pandas', 'scikit-image',
                       'rasterio>=1.0.7', 'opencv-python',
-                      'spacenetutilities==3.0', 'keras>=2.2.2',
+                      'spacenetutilities', 'keras>=2.2.2',
                       'tensorflow>=1.10.0', 'tensorboard>=1.10.0',
                       'setuptools'],  # TODO: UPDATE
     scripts=[
-            'bin/make_np_arrays',
-            'bin/train_model',
-            'bin/make_predictions0'
+            'bin/make_np_arrays.py',
+            'bin/train_model.py',
+            'bin/make_predictions.py'
         ],
 
     package_data={
